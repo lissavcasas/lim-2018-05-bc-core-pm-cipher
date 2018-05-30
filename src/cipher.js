7e940a4c81e69f390e5510c2f493c93bdf960687
marimeli <marimel.casas@gmail.com>
2018-05-30 17:05:03 -0500

    actualizando a gh-pages

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,50 @@
-//window.cipher = {
-  // ... 
-//};
+///METODO CIFRAR  
+window.cipher = {
+  //Declarando función para descifrar una cadena de texto
+  decode: (key,string) => {
+    //parsear para verificar que sea un valor numérico 
+    const offsetUser = parseInt(key.value); 
+    // almacenar el valor del mensaje ingresado
+    const stringUser = string.value; 
+    // almacenar el valor del string Cifrado
+    let myResult = ''; 
 
-//Declarando función para cifrar una cadena de texto
-const cipher = () => {
-  // parsear para verificar que sea un valor numérico 
-  const offSet = parseInt(keyInput.value); 
-  // almacenar el valor del mensaje ingresado
-  const asciiNumber = messageInput.value; 
-  // almacenar el valor del string Cifrado
-  var myStringCipher = ""; 
+    for(let i = 0; i < stringUser.length; i++) {  // recorrer el string del usuario
+      let asciiCharacter = stringUser.charCodeAt(i); //convertir el string del usuario en un caracter ASCII
+      let decodeValue;
+      if(asciiCharacter >= 65 && asciiCharacter <= 90) { //preguntar si es una letra mayúscula en el código ASCII
+        decodeValue = (asciiCharacter - 65 + offsetUser) %26 + 65; //obtener el número de la letra en el codigo ASCII
+      } else if(asciiCharacter >= 97 && asciiCharacter <= 122) { //preguntar si es una letra minúscula en el código ASCII 
+        decodeValue = (asciiCharacter - 97 + offsetUser) %26 + 97;
+      } else {
+        decodeValue= asciiCharacter;
+      } 
+        myResult += String.fromCharCode(decodeValue);
+    } 
+    return myResult;
+  },
+  ///METODO CIFRAR 
+  encode: (key,string) => {
+    // parsear para verificar que sea un valor numérico 
+    const offsetUser = parseInt(key.value); 
+    // almacenar el valor del mensaje ingresado
+    const stringUser = string.value; 
+    // almacenar el valor del string Cifrado
+    let myResult = ''; 
 
-  for(let i = 0; i < asciiNumber.length; i++){  // recorrer el string del usuario
-      var numberOfTheLetter = (asciiNumber.charCodeAt(i)-65+offSet)%26+65; //obtener el número de la letra en el codigo ASCII
-      myStringCipher += String.fromCharCode(numberOfTheLetter);  // convertir codigo ASCII en una letra
-  } 
-  document.getElementById("myStringCipher").innerHTML=myStringCipher; // Pedir el ID donde se imprimirá el mensaje cifrado
-}
- 
-//Declarando función para descifrar una cadena de texto
+    for(let i = 0; i < stringUser.length; i++) {  // recorrer el string del usuario
+      let asciiCharacter = stringUser.charCodeAt(i); //convertir el string del usuario en un caracter ASCII
+      let encodeValue;
+      if(asciiCharacter >= 65 && asciiCharacter <= 90) { //preguntar si es una letra mayúscula en el código ASCII
+        encodeValue = (asciiCharacter - 65 - offsetUser + 26) %26 + 65; //obtener el número de la letra en el codigo ASCII
+      } else if(asciiCharacter >= 97 && asciiCharacter <= 122) { //preguntar si es una letra minúscula en el código ASCII 
+        encodeValue = (asciiCharacter - 97 - offsetUser + 26) %26 + 97;
+      } else {
+        encodeValue= asciiCharacter;
+      } 
+        myResult += String.fromCharCode(encodeValue);
+    } 
+    return myResult;
+  },
+//createCipherWithOffset: () => {
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 //Llamando a los elmentos del HTML que se van a manipular
-const messageInput = document.getElementById('message'); // almacenar el mensaje ingresado
-const buttonCipher = document.getElementById('buttonCode'); // almacenar el bottón que permite cifrar
-const keyInput = document.getElementById('offSet'); // almacenar la cantidad que se ingresa para el desplazamiento
+const myString = document.getElementById('my_string'); // almacenar el mensaje ingresado
+const offset = document.getElementById('key_number'); // almacenar la cantidad que se ingresa para el desplazamiento
+const buttonDecode = document.getElementById('button_decoder'); // almacenar el bottón que permite cifrar
+const buttonEncode = document.getElementById('button_encoder'); // almacenar el bottón que permite descifrar
 
-//Llamando a la función cifrar
-buttonCipher.addEventListener('click', cipher); //Al escuchar el click del botón, ejecuta la función cifrar
+//Llamando a la función cifrar buttonDecode.addEventListener('click', decode); 
+//Agregando evento: Botón codificar
+const startDecode = () => { 
+    let result = cipher.decode(offset,myString);
+    document.getElementById('my_result').innerHTML = result; // Pedir el ID donde se imprimirá el mensaje cifrado
+}
+buttonDecode.addEventListener('click', startDecode); //Al escuchar el click del botón, ejecutar la función cifrar
+
+//Agregando evento: Botón decodificar
+const startEncode = () => { 
+    let result = cipher.encode(offset,myString);
+    document.getElementById('my_result').innerHTML = result; // Pedir el ID donde se imprimirá el mensaje descifrado
+}
+buttonEncode.addEventListener('click', startEncode); //Al escuchar el click del botón, ejecutar la función descifrar
