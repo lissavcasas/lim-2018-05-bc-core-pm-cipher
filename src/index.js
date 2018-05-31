e829f7c3844d718c4d57500474801e9066db34d1
marimeli <marimel.casas@gmail.com>
2018-05-31 18:05:32 -0500

    nuevos cambios logica tested

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const myString = document.getElementById("my_string"); // almacenar el mensaje i
 const myOffset = document.getElementById("key_number"); // almacenar la cantidad que se ingresa para el desplazamiento
 const buttonEncode = document.getElementById("button_encoder"); // almacenar el bottón que permite cifrar
 const buttonDecode = document.getElementById("button_decoder"); // almacenar el bottón que permite descifrar
-const result = document.getElementById("my_result"); // almacenar el resultado
+const result = document.getElementById("my_result"); // almacenar el bottón que permite descifrar
 
 //Agregando evento: Botón cifrar
 const encoded = () => { 
-  result.innerHTML = window.cipher.encode(myOffset.value,myString.value);
+  result.value = window.cipher.encode(myOffset.value,myString.value);
 }
 buttonEncode.addEventListener("click", encoded); //Al escuchar el click del botón, ejecutar la función cifrar
 
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -28,6 +28,22 @@ describe('cipher', () => {
     });
   });
 
+  describe('cipher.decode', () => {
+
+    it('debería ser una función', () => {
+      assert.equal(typeof cipher.decode, 'function');
+    });
+
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      let result = cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG");
+      assert.equal(result, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
+  });
+  cons 
+  //else if(asciiCharacter >= 97 && asciiCharacter <= 122) { //preguntar si es una letra minúscula en el código ASCII//
+  //for(let i = 0; i < string.length; i++) {
+
+
   describe('cipher.createCipherWithOffset', () => {
 
     xit('debería ser una función', () => {
