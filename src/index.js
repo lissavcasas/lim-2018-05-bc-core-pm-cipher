110e132992b81b7df1d80d675c535638119cbdae
marimeli <marimel.casas@gmail.com>
2018-06-01 15:18:50 -0500

    tested 100%

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const myString = document.getElementById("my_string"); // almacenar el mensaje i
 const myOffset = document.getElementById("key_number"); // almacenar la cantidad que se ingresa para el desplazamiento
 const buttonEncode = document.getElementById("button_encoder"); // almacenar el bottón que permite cifrar
 const buttonDecode = document.getElementById("button_decoder"); // almacenar el bottón que permite descifrar
-const result = document.getElementById("my_result"); // almacenar el mensaje resultado.
+const result = document.getElementById("my_result"); // almacenar el mensaje resultado
 
 //Agregando evento: Botón cifrar
 const encode = () => { 
